a1db96431a6c7a414731c9f2d2404a73dd9a5689
filippoesfea <116521875+filippoesfea@users.noreply.github.com>
2022-12-18 14:05:55 +0100

    Create get_most_common_words.py
    If you have any other way to improve code, let's do it

---
+++ get_most_common_words.py
@@ -0,0 +1,14 @@
+import collections
+# optional - import json
+# optional - import re
+
+# open file (if directory is not specified in path file, insert complete path in directory)
+# es. example_mongo --> AAA --> 'Desktop\.....
+file = open('AAA.json', 'rt')
+data = file.read()
+# splitting each content in file
+words = data.split()
+# collections
+# most common --> select how much words get
+most_common = collections.Counter(words).most_common(3)
+print(most_common)
